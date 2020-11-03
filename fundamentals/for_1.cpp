155ffd626501206ebe9e1335363da85a8c6cbda0
Abe <abelardobandeirajr@gmail.com>
2020-11-03 01:01:11 -0300

    update

---
+++ fundamentals/for_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+
+int i = 0;
+
+for(int i = 1; i <= 10; i++) {
+    cout << i << endl;
+    i++;
+}
+
+
+
+    for(int j = 100; j >= 0; j -=2) {
+        cout << j << " ";
+    }
+
+    return 0;
+
+}
